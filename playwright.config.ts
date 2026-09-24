bbd14d1c0a391251df698906d60d2e17f055aab9
Dillon Lui <dillonlui@gmail.com>
2026-09-24 07:45:59 -0500

    Build before Playwright in Pages workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,8 +37,11 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'npm run build && ASTRO_PREVIEW_BACKGROUND=1 npm run preview',
+    command: process.env.CI
+      ? 'npm run preview'
+      : 'npm run build && ASTRO_PREVIEW_BACKGROUND=1 npm run preview',
     url: 'http://localhost:4321',
     reuseExistingServer: !process.env.CI,
+    timeout: 120_000,
   },
 });
